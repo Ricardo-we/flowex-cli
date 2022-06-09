202a5682f61bafc82ab140a57ad6dc3c055bc966
Ricardo-we <totgamer75@gmail.com>
2022-06-09 10:13:03 -0600

    db.sqlite in root directory

--- src/utils/fileTemplateStrings/initialize-project-templates.js
+++ src/utils/fileTemplateStrings/initialize-project-templates.js
@@ -23,7 +23,7 @@ const appDbConfigTemplate = `
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: '../db.sqlite'
+    storage: 'db.sqlite'
 });
 
 const authenticate = async () => {
